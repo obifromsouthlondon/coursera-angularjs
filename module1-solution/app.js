fc60092c404148dedbfbf1177bee31c0b6c569d1
oorjiekwe <oorjiekwe@conversity.com>
2019-03-10 20:34:34 +0000

    Added style for input border and message text

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,9 +8,22 @@
   {
     $scope.message = "";
     $scope.itemsForLunch = "";
+
+    $scope.style = {};
+    $scope.colour = {};
+
     $scope.checkIfTooMuch = function()
     {
-      if ($scope.itemsForLunch.split(",").filter(String).length <= 3)
+      $scope.style =  {border:"1px solid green"};
+      $scope.colour = {color:'green'};
+
+      if ($scope.itemsForLunch === "")
+      {
+        $scope.message = "Please enter data first";
+        $scope.style =  {border:"1px solid red"};
+        $scope.colour = {color:'red'};
+      }
+      else if ($scope.itemsForLunch.split(",").filter(String).length <= 3)
       {
         $scope.message = "Enjoy!";
       }

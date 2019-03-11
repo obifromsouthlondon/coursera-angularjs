2e469fc880951106945ad6dd192cc657f5b6813f
oorjiekwe <oorjiekwe@conversity.com>
2019-03-11 11:04:30 +0000

    no changes

--- Lecture11/app.js
+++ Lecture11/app.js
@@ -1,22 +1,17 @@
 (function() {
-  'use strict';
-  angular.module("MsgApp", [])
-  .controller("MsgController", MsgController);
+  "use strict";
+  angular.module("MsgApp", []).controller("MsgController", MsgController);
 
-  MsgController.$inject = ["$scope"]
-  function MsgController($scope)
-  {
+  MsgController.$inject = ["$scope"];
+  function MsgController($scope) {
     $scope.name = "MsgApp";
     $scope.stateOfBeing = "hungry";
-    $scope.sayMessage = function()
-    {
+    $scope.sayMessage = function() {
       return "Obi likes to eat healthy";
     };
 
-    $scope.feedObi = function()
-    {
+    $scope.feedObi = function() {
       $scope.stateOfBeing = "fed";
     };
   }
-
-}());
+})();
--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,37 +1,33 @@
 (function() {
-  'use strict';
-  angular.module("LunchCheck", [])
-  .controller("LunchCheckController", LunchCheckController);
+  "use strict";
+  angular
+    .module("LunchCheck", [])
+    .controller("LunchCheckController", LunchCheckController);
 
   LunchCheckController.$inject = ["$scope"];
-  function LunchCheckController($scope)
-  {
+  function LunchCheckController($scope) {
     $scope.message = "";
     $scope.itemsForLunch = "";
-
     $scope.style = {};
     $scope.colour = {};
 
-    $scope.checkIfTooMuch = function()
-    {
-      $scope.style =  {border:"1px solid green"};
-      $scope.colour = {color:'green'};
+    $scope.checkIfTooMuch = function() {
+      $scope.style = { border: "1px solid green" };
+      $scope.colour = { color: "green" };
 
-      if ($scope.itemsForLunch === "")
-      {
+      if ($scope.itemsForLunch === "") {
         $scope.message = "Please enter data first";
-        $scope.style =  {border:"1px solid red"};
-        $scope.colour = {color:'red'};
-      }
-      else if ($scope.itemsForLunch.split(",").filter(String).length <= 3)
-      {
+        $scope.style = { border: "1px solid red" };
+        $scope.colour = { color: "red" };
+      } else if ($scope.itemsForLunch.split(",").filter(String).length <= 3) {
         $scope.message = "Enjoy!";
-      }
-      else
-      {
+      } else {
         $scope.message = "Too much!";
       }
     };
-  };
+  }
+})();
 
-}());
+function myAlert() {
+  alert("Hey!");
+}
